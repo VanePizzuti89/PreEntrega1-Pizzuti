66d74b29d1dca2ca9c9df17a93edde561d422f6d
VanePizzuti89 <119458151+VanePizzuti89@users.noreply.github.com>
2023-05-02 20:36:30 -0300

    App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import NavBar from "./components/NavBar/NavBar";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
-import Footer from "./components/Footer";
 import ItemCount from "./components/ItemCount/ItemCount"
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer'
-import { BrowserRouter } from "react-router-dom";
-
+import { BrowserRouter, Router, Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
 
 
 
@@ -13,15 +12,23 @@ const App = () => {
     <BrowserRouter>
 
     <NavBar/>
-    <ItemListContainer greeting={"Bienvenidos"}/>
-    <ItemDetailContainer/>
-    <ItemCount initial={1} stock={10} onAdd={(quantity) => console.log("Cantidad agregada", quantity)}/>
-  
-
+    
+    <Routes>
+      <Route path='/' element={<ItemListContainer/>}/>
+      <Route path='/category/:categoryId' element={<ItemListContainer/>}/>
+      <Route path='/item/:itemId' element={<ItemDetailContainer/>}/>
+      <Route path='*' element={<h1>404 NOT FOUND</h1>}/>
+    </Routes>
     <Footer/>
 
-
     </BrowserRouter>
+   
+  
+    //<ItemCount initial={1} stock={10} onAdd={(quantity) => console.log("Cantidad agregada", quantity)}/>
+  
+
+
+   
 
   )
 
