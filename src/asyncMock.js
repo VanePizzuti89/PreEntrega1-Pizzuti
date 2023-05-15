dd6e9727d732859d80d3ca5dd007ffe6d04abd5d
VanePizzuti89 <119458151+VanePizzuti89@users.noreply.github.com>
2023-05-15 20:37:25 -0300

    Nuevas cards

--- src/asyncMock.js
+++ src/asyncMock.js
@@ -26,7 +26,36 @@ const products = [
         img: 'https://i.postimg.cc/VLTqKqnt/gorra-starwars.jpg',
         stock: 25,
         description: 'Gorra Star Wars color negro y blanco'
+    },
+    {
+        id: '4',
+        name: 'Remera Nonpalidece',
+        price: 2000,
+        category: "remeras",
+        img: 'https://http2.mlstatic.com/D_NQ_NP_775187-MLA31128313558_062019-O.webp',
+        stock: 28,
+        description: 'Remera de la banda Nonpalidece, del disco llamado Activistas'
+
+    },
+    {
+        id: '5',
+        name: 'Taza Messi',
+        price: 1800,
+        category: "tazas",
+        img: 'https://i.postimg.cc/LXRBzmf0/taza-messi.jpg',
+        stock: 20,
+        description: 'Taza Que miras bobo?'
+    },
+    {
+        id: '6',
+        name: 'Gorra Star Wars',
+        price: 2000,
+        category: "gorras",
+        img: 'https://i.postimg.cc/VLTqKqnt/gorra-starwars.jpg',
+        stock: 25,
+        description: 'Gorra Star Wars color negro y blanco'
     }
+
 ]
 
 export const getProducts = () => {
