a5640626923d2390e13087a7cb845425ed4ba92f
VanePizzuti89 <119458151+VanePizzuti89@users.noreply.github.com>
2023-05-21 14:07:22 -0300

    Cards nuevas

--- src/asyncMock.js
+++ src/asyncMock.js
@@ -2,58 +2,84 @@ const products = [
     {
         id: '1',
         name: 'Remera Capitán América',
-        price: 2000,
+        price: 5500,
         category: "remeras",
-        img: 'https://i.postimg.cc/DZqpTCQR/rem-captamerica.jpg',
-        stock: 28,
-        description: 'Remera Capitán América color azul'
-
+        img: 'https://i.postimg.cc/3Ry8b4pv/rem-captamerica.jpg',
+        stock: 15,
+        description: 'Remera del Capitán América, color de fondo azul'
     },
     {
         id: '2',
         name: 'Taza Messi',
-        price: 1800,
+        price: 3000,
         category: "tazas",
-        img: 'https://i.postimg.cc/LXRBzmf0/taza-messi.jpg',
+        img: 'https://i.postimg.cc/3RDhZM5j/taza-messi.jpg',
         stock: 20,
-        description: 'Taza Que miras bobo?'
+        description: 'Taza de Messi con frase del Mundial 2022'
     },
     {
         id: '3',
         name: 'Gorra Star Wars',
-        price: 2000,
+        price: 2600,
         category: "gorras",
         img: 'https://i.postimg.cc/VLTqKqnt/gorra-starwars.jpg',
         stock: 25,
-        description: 'Gorra Star Wars color negro y blanco'
+        description: 'Gorra de Star Wars color negro y blanco'
     },
     {
         id: '4',
         name: 'Remera Nonpalidece',
-        price: 2500,
+        price: 5500,
         category: "remeras",
         img: 'https://i.postimg.cc/9fbNY2PK/remera-nonpalidece.jpg',
         stock: 28,
-        description: 'Remera de Nonpalidece, disco llamado Activistas'
+        description: 'Remera de Nonpalidece, disco llamado Activistas, color amarrillo'
 
     },
     {
         id: '5',
         name: 'Taza River',
-        price: 1800,
+        price: 3000,
         category: "tazas",
-        img: 'https://i.postimg.cc/Bvht98cg/taza-river.jpg',
+        img: 'https://i.postimg.cc/8C6XKTcC/taza-river.jpg',
         stock: 20,
         description: 'Taza del Club Atlético River Plate'
     },
     {
         id: '6',
+        name: 'Gorra Patricio Rey',
+        price: 2600,
+        category: "gorras",
+        img: 'https://i.postimg.cc/7YrDdB88/gorra-losredondos.jpg',
+        stock: 25,
+        description: 'Gorra de Patricio Rey, color de fondo negro'
+    },
+    {
+        id: '7',
+        name: 'Remera Dragon Ball',
+        price: 5500,
+        category: "remeras",
+        img: 'https://i.postimg.cc/9QQ1Jhfx/remera-dragonball.jpg',
+        stock: 15,
+        description: 'Remera de Dragon Ball, color naranja'
+    },
+    {
+        id: '8',
         name: 'Taza Boca',
-        price: 2000,
+        price: 3000,
         category: "tazas",
-        img: 'https://i.postimg.cc/FFTk5BGv/taza-boca.jpg',
+        img: 'https://i.postimg.cc/kg2GVpYC/taza-boca.jpg',
         stock: 25,
         description: 'Taza del Club Atlético Boca Juniors'
+    },
+    {
+        id: '9',
+        name: 'Gorra Adidas',
+        price: 2600,
+        category: "gorras",
+        img: 'https://i.postimg.cc/cLqZqXpG/gorra-adidas.jpg',
+        stock: 25,
+        description: 'Gorra de la marca Adidas, de color de fondo negro'
     }
 
 ]
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -7,7 +7,7 @@ const NavBar = () => {
     return (
         <nav className="NavBar">
             <Link to="/">
-            <h1 className="nav__title">Almitallia Estampados</h1>
+            <h1 className="nav__title">Almitallia Sublimaciones</h1>
             </Link>
             
             <div className="categories">
