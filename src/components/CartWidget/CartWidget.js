f8ed64adb8b6a62d9cd7d7a13a3a074cd6777aef
VanePizzuti89 <119458151+VanePizzuti89@users.noreply.github.com>
2023-05-25 21:00:45 -0300

    Cart

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -1,12 +1,18 @@
-
 import './CartWidget.css'
+import cart from '/images/carrito.png'
+import { useContext} from 'react'
+import { CartContext } from '../../context/CartContext'
+import { Link } from 'react-router-dom'
+
 
 const CartWidget = () => {
+    const { totalQuantity } = useContext(CartContext)
+
     return (
-        <div className="carrito-style">
-            <img className="img-carrito" src="/images/carrito.png" alt="carrito-widget"></img>
-            <p className='carri-number'>0</p> 
-        </div>  
+        <Link to='/cart' className="carrito-stile" style={{display: totalQuantity > 0 ? 'block' : 'none'}}>
+            <img className="img-carrito" src={cart} alt="carrito-widget"></img>
+            { totalQuantity }
+        </Link>  
     )
 }
 
