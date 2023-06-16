8e2aea3323c8b91c9e0a5f4cf4fa141a08ce8968
VanePizzuti89 <119458151+VanePizzuti89@users.noreply.github.com>
2023-06-16 15:43:43 -0300

    NavBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -8,15 +8,13 @@ const NavBar = () => {
         <nav className="NavBar">
             <Link to="/">
             <img className="logo_alm" src="./images/logo-am.png" alt="logo"/>    
-            
             </Link>
             
             <div className="categories">
-            <NavLink to="/category/remeras" className="nav__link link">Remeras</NavLink>
-            <NavLink to="/category/tazas" className="nav__link link">Tazas</NavLink>
-            <NavLink to="/category/gorras" className="nav__link link">Gorras</NavLink>
+            <NavLink to="/category/remeras" className="nav__link link">REMERAS</NavLink>
+            <NavLink to="/category/tazas" className="nav__link link">TAZAS</NavLink>
+            <NavLink to="/category/gorras" className="nav__link link">GORRAS</NavLink>
             <NavLink to="/cart" className="nav__link link"><CartWidget/></NavLink>
-                
             </div>
             
         </nav>
